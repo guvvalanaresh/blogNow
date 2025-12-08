ec07ce9fae36204a19b8554f2ffe27bf55ace22e
guvvalanaresh <gnareshkumarreddy7@gmail.com>
2025-12-08 17:42:04 +0530

    designed BlogCard.jsx component and Pagination.jsx components and finisheddesigning the Home.jsx Page

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import '../App.css'
 
 const BlogCard = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='grid grid-cols-[2fr_1fr] gap-4 p-3 rounded-lg bg-white shadow-xs border-gray-300'>
+      {/* Content */}
+      <div className='flex flex-col p-2'>
+        {/* Date details */}
+          <ul className='flex text-xs gap-1 mb-3 text-gray-500 nunito'>
+            <li>Design</li>&bull;
+            <li>8 min read</li>&bull;
+            <li>Oct 26, 2023</li>
+          </ul>
+        {/* Title & Description */}
+        <div className='flex flex-col justify-between mb-3'>
+          <h1 className='mb-2 text-xl font-bold archivo-black-regular'>The Future of UI Design: Trends to Watch in 2024</h1>
+          <p className='line-clamp-2 nunito text-gray-500'>A deep dive into the most anticipated user interface trends of the upcoming year, from AI integration to neo-brutalism.</p>
+        </div>
+        {/* Button */}
+        <div>
+          <button className='px-4 py-2 rounded-sm text-sm font-semibold bg-gray-100 cursor-pointer hover:bg-gray-200'>Read More</button>
+        </div>
+      </div>
+      {/* Image */}
+      <div className='h-full'>
+        <img src="src/assets/images/blog-image.jpg" alt="Blog Image" className='w-full h-full object-cover rounded-md' />
+      </div>
+    </section>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,31 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import CategoryCard from '../components/CategoryCard'
 import '../App.css'
+import BlogCard from '../components/BlogCard'
+import Pagination from '../components/Pagination'
 
 const Home = () => {
   return (
-    <div className='bg-[#F0F0F0] h-screen'>
+    <div className='bg-[#F0F0F0]'>
       <Navbar />
       <div className='grid grid-cols-[250px_1fr] gap-4 ml-8 mt-6'>
         <div>
           <CategoryCard />
         </div>
-        <div className='flex flex-col mb-2'>
-          <h1 className='text-3xl font-bold archivo-black-regular tracking-tight'>From the Blog</h1>
-          <p className='nunito mt-2 text-gray-500'>The latest news,tips and insights from our team</p>
-        </div>
+        {/* The main Page content goes here */}
         <div>
-          {/* Blog card component */}
+          <div className='flex flex-col mb-2'>
+            <h1 className='text-3xl font-bold archivo-black-regular tracking-tight'>From the Blog</h1>
+            <p className='nunito mt-1 text-gray-500'>The latest news,tips and insights from our team</p>
+          </div>
+          <div className='flex flex-col gap-4 mr-4 mt-8 mb-10'>
+            {/* Blog card component */}
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <Pagination />
+          </div>
         </div>
       </div>
     </div>
