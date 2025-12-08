cbe73bec0e99cadb1d6eda34639dbed8da46e7af
guvvalanaresh <gnareshkumarreddy7@gmail.com>
2025-12-08 12:54:27 +0530

    Done with setting up the Home Page and the components

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -3,13 +3,13 @@ import '../App.css'
 
 const CategoryCard = () => {
 
-  const categoryClassName = 'px-3 py-2 rounded-sm hover:bg-[#B4C6FC] hover:text-blue-700';
+  const categoryClassName = 'px-3 py-2 rounded-sm nunito hover:bg-[#B4C6FC] hover:text-blue-700';
 
   return (
-    <section className='w-60 px-3 py-1 rounded-sm inter border-gray-300'>
+    <section className='w-60 px-3 py-1 rounded-sm shadow-2xs border-gray-300 bg-white'>
       <div className='flex flex-col mt-3'>
         <h1 className='text-sm font-bold'>Categories</h1>
-        <p className='text-sm text-gray-500'>Filter by topic</p>
+        <p className='text-sm text-gray-500 ninito'>Filter by topic</p>
       </div>
       <div className='mt-5 mb-2'>
         <ul className='flex flex-col text-sm'>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import CategoryCard from '../components/CategoryCard'
+import '../App.css'
 
 const Home = () => {
   return (
-    <div>
+    <div className='bg-[#F0F0F0] h-screen'>
       <Navbar />
-      <CategoryCard />
+      <div className='grid grid-cols-[250px_1fr] gap-4 ml-8 mt-6'>
+        <div>
+          <CategoryCard />
+        </div>
+        <div className='flex flex-col mb-2'>
+          <h1 className='text-3xl font-bold archivo-black-regular tracking-tight'>From the Blog</h1>
+          <p className='nunito mt-2 text-gray-500'>The latest news,tips and insights from our team</p>
+        </div>
+        <div>
+          {/* Blog card component */}
+        </div>
+      </div>
     </div>
   )
 }
