2f8b4b3cf08e38d21a7d3492712215ac6352da49
guvvalanaresh <gnareshkumarreddy7@gmail.com>
2025-12-09 11:56:16 +0530

    made some fixes to styling in the create blog page

--- src/components/BlogCreations/Navbar.jsx
+++ src/components/BlogCreations/Navbar.jsx
@@ -3,22 +3,24 @@ import '../../App.css';
 
 const Navbar = () => {
   return (
-      <nav className='flex justify-between items-center bg-[#F0F0F0] p-2 border-b border-gray-300 inter'>
-        <div className='flex items-center gap-1 ml-3'>
-            <a href="#"><img src="/blog-logo.png" alt="Blog Logo" className='w-10 h-10' /></a>
-            <span className='roboto-mono !font-bold'>blogNow</span> 
-        </div>
-        {/* Right Side */}
-        <div className="flex items-center gap-3 mr-3">
-            <ul className='flex gap-5 text-sm'>
-                <a href="#" className='hover:bg-gray-300 px-2 py-1 rounded-xs'><li>Dashboard</li></a>
-                <a href="#" className='hover:bg-gray-300 px-2 py-1 rounded-xs'><li>View Posts</li></a>
-            </ul>
-            <button className="px-3 py-1.5 text-sm rounded-md bg-[#3B82F6] hover:bg-[#2563EB] text-white font-semibold cursor-pointer">
-                New Post
-            </button>
-        </div>
-      </nav>
+      <header className='w-full flex justify-center'>
+        <nav className='flex justify-between w-[70%] max-w-6xl items-center p-2 border-b border-gray-300 inter'>
+            <div className='flex items-center gap-1 ml-3'>
+                <a href="#"><img src="/blog-logo.png" alt="Blog Logo" className='w-10 h-10' /></a>
+                <span className='roboto-mono font-bold!'>blogNow</span> 
+            </div>
+            {/* Right Side */}
+            <div className="flex items-center gap-3 mr-3">
+                <ul className='flex gap-5 text-sm'>
+                    <a href="#" className='hover:bg-gray-300 px-2 py-1 rounded-xs'><li>Dashboard</li></a>
+                    <a href="#" className='hover:bg-gray-300 px-2 py-1 rounded-xs'><li>View Posts</li></a>
+                </ul>
+                <button className="px-3 py-1.5 text-sm rounded-md bg-[#3B82F6] hover:bg-[#2563EB] text-white font-semibold cursor-pointer">
+                    New Post
+                </button>
+            </div>
+        </nav>
+      </header>
   )
 }
 
--- src/pages/CreateBlogPage.jsx
+++ src/pages/CreateBlogPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../components/BlogCreations/Navbar'
+import '../App.css'
 
 const CreateBlogPage = () => {
   return (
-    <div>
+    <div className='bg-[#F0F0F0] h-screen'>
       <Navbar />
+      <section>
+        <div className='w-[70%] max-w-6xl mx-auto flex flex-col gap-4 mt-8'>
+           <h1 className='text-2xl anton-regular font-extrabold!'>Create New Post</h1>
+        </div>
+      </section>
     </div>
   )
 }
